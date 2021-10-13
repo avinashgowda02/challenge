66af9a630c75d7b7e5ab06f17a7eff7b2b84d66a
avinashgowda02 <44193354+avinashgowda02@users.noreply.github.com>
2021-10-13 18:50:00 +0530

    Update metadatascript.sh

--- metadatascript.sh
+++ metadatascript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-metadata=`curl -s http://169.254.169.254/latest/meta-data/`
-echo $metadata 
+curl -s http://169.254.169.254/latest/meta-data/
+echo ""
 echo  "enter the metadata value you want to output from above output"
 read value
 output=`curl -s http://169.254.169.254/latest/meta-data/$value`
